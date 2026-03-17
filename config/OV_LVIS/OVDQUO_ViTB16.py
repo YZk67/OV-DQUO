ccf4a93e6f8eb426b62843fbc15186939d20deca
YZk67 <kirazhang86@gmail.com>
2026-03-17 11:06:19 -0700

    eval_tau

--- config/OV_LVIS/OVDQUO_ViTB16.py
+++ config/OV_LVIS/OVDQUO_ViTB16.py
@@ -127,7 +127,7 @@ multi_prompt_text_embed = "pretrained/lvis_multi_prompt_evaclip_vitb_16.pt"
 
 
 ##### start inference parameters ##### 
-eval_tau = 100
+eval_tau = 1
 objectness_alpha = 1.0
 nms_iou_threshold = 0.5
 target_class_factor=8.0
